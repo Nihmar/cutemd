d51cb88dc97490aeff5df0be1aa912a6a4d6d341
nihmar <manente.alessandro@proton.me>
2026-06-22 23:01:45 +0200

    Restore debug HTML dump

--- ui/editor_tab.py
+++ ui/editor_tab.py
@@ -854,6 +854,9 @@ class EditorTab(QWidget):
             "</body>\n</html>"
         )
 
+        _DUMP = Path.home() / "preview_dump.html"
+        _DUMP.write_text(html, encoding="utf-8")
+
         self._syncing_scroll += 1
         self.preview.setHtml(html)
         self._syncing_scroll -= 1
